8fe4e83551c734969fd992f2ced47f4c2f042a2d
Zain Fathoni <zain.fathoni@gmail.com>
2020-12-26 09:32:34 +0800

    Fix tests by mocking IntersectionObserver
    https://github.com/thebuilder/react-intersection-observer/blob/master/src/test-utils.ts

--- src/screens/__test__/app.test.js
+++ src/screens/__test__/app.test.js
@@ -2,6 +2,7 @@ import { render, screen, waitForLoadingToFinish, act } from 'test/test-utils';
 import { server, rest } from 'test/server';
 import userEvent from '@testing-library/user-event';
 import App from 'screens/app';
+import { mockAllIsIntersecting } from 'test/intersection-observer-test-utils';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -9,6 +10,8 @@ const renderAppScreen = async ({ username } = {}) => {
   jest.useFakeTimers();
 
   const utils = render(<App />);
+
+  mockAllIsIntersecting(true);
 
   if (!username) {
     username = 'USERNAME';
